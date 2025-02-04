4b90c884f7d13ad942df58c6bea564d1fd6bb4a8
PauloJankosz <paulohenrique_j@icloud.com>
2025-02-04 14:34:29 -0300

    Criação do projeto em React

---
+++ Desafio/my-todo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Começo do projeto</h1>
+        <h1>Teste inicial</h1>
+        <h1>Mais um teste</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
